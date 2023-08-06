7d5ac40dc283dcd415c34d7ac29ae6f5d69c2f84
Stanislav Shkradov <studingdev@gmail.com>
2023-08-06 09:19:04 +0300

    result

--- src/Components/description/about/about.jsx
+++ src/Components/description/about/about.jsx
@@ -6,8 +6,8 @@ export default function About () {
     return (
         <div className="about">
             <h2 className="about__header">ABOUT ME</h2>
-            <p className="about__intro">Hello, I’m Robert Smith</p>
-            <p className="about__story">Hello! I’m Robert Smith. Senior Web Developer specializing in front end development. Experienced with all stages of the development cycle for dynamic web projects. Well-versed in numerous programming languages including JavaScript, SQL, and C. Stng background in project management and customer relations.</p>
+            <p className="about__intro">Hello, I’m Stanislav Shkradov</p>
+            <p className="about__story">Hello! I’m Stanislav Shkradov, a beginner web developer. Currently, I know well html, css and JS, I study React, SCSS. I have not yet participated in the development of commercial projects, but I am striving in this direction. I also have a little practice and understanding of the interaction between the work of the client side and the server side.</p>
         </div>
     )
 }
